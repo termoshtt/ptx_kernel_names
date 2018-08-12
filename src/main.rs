cd72212a86f1c9b5255504e264b6e69df80da1a1
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-08-12 19:37:45 +0900

    MemoryBuffer

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,58 @@
 extern crate llvm_sys;
 #[macro_use]
 extern crate structopt;
+extern crate failure;
 
-use std::path::*;
+use failure::err_msg;
+use llvm_sys::core::*;
+use llvm_sys::prelude::*;
+use std::ffi::*;
+use std::os::raw::c_char;
+use std::ptr::null_mut;
 use structopt::StructOpt;
+
+type Result<T> = ::std::result::Result<T, failure::Error>;
 
 #[derive(StructOpt, Debug)]
 struct Opt {
     /// Input file
-    #[structopt(parse(from_os_str))]
-    input: PathBuf,
+    input: String,
 
     /// Output file
-    #[structopt(parse(from_os_str))]
-    output: PathBuf,
+    output: String,
+}
+
+struct MemoryBuffer(LLVMMemoryBufferRef);
+
+impl Drop for MemoryBuffer {
+    fn drop(&mut self) {
+        unsafe { LLVMDisposeMemoryBuffer(self.0) }
+    }
+}
+
+impl MemoryBuffer {
+    fn new(filename: &str) -> Result<Self> {
+        let input = CString::new(filename)?;
+        let mut membuf: LLVMMemoryBufferRef = null_mut();
+        let mut msg: *mut c_char = null_mut();
+        let result = unsafe {
+            LLVMCreateMemoryBufferWithContentsOfFile(
+                input.into_raw(),
+                &mut membuf as *mut LLVMMemoryBufferRef,
+                &mut msg as *mut *mut c_char,
+            )
+        };
+        if result != 0 {
+            let msg = unsafe { CString::from_raw(msg) };
+            return Err(err_msg(format!("Canont read input: {:?}", msg)));
+        }
+        Ok(MemoryBuffer(membuf))
+    }
 }
 
 fn main() {
     let opt = Opt::from_args();
     println!("{:?}", opt);
+
+    let _membuf = MemoryBuffer::new(&opt.input);
 }
